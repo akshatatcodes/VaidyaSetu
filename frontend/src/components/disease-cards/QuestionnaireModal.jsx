1c104a4ed19f817fa2f0a80a4e36cc2d5b6dbc1c
akshatatcodes <akshatcodesat@gmail.com>
2026-04-16 14:03:08 +0530

    Fix import path for API_URL in QuestionnaireModal

--- frontend/src/components/disease-cards/QuestionnaireModal.jsx
+++ frontend/src/components/disease-cards/QuestionnaireModal.jsx
@@ -7,7 +7,7 @@ import {
 import axios from 'axios';
 import { useTranslation } from 'react-i18next';
 
-import { API_URL } from '../config/api';
+import { API_URL } from '../../config/api';
 
 const QuestionnaireModal = ({ isOpen, onClose, diseaseId, currentScore, profile, onScoreUpdate }) => {
   const { t } = useTranslation();
